4256ba06c3f468703ba26dbe6c455bbd9731d2f8
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-15 12:06:47 +0100

    re-init

--- build.js
+++ build.js
@@ -1,16 +1,68 @@
 const { join } = require('path');
 const fs = require('fs');
 
-const ff = require('./ff');
-const chrome = require('./ff');
+var mdn = require('css-tree/data/mdn-data-properties.json');
+var R = require('ramda');
 
-// chrome has prio over ff
-const merged = Object.assign({}, ff, chrome);
+const fn = R.pipe(...[
+  R.toPairs,
 
-const result = `module.exports = ${JSON.stringify(merged, null, 2)};`;
+  R.map(R.zipObj(['prop', 'val'])),
 
 
-fs.writeFile(join(__dirname, 'index.js'), result, 'utf8', (err, res) => {
+  R.filter(
+    R.pathEq(['val', 'inherited'], true)
+  ),
+
+  R.map(R.over(
+    R.lensProp('val'),
+    R.prop('initial')
+  )),
+
+
+  R.map(R.pipe(
+    R.values,
+    R.zipObj(['prop', 'initial'])
+  )),
+
+
+  R.map(R.over(
+    R.lensProp('initial'),
+    R.when(
+      R.is(String),
+      R.replace(/<\/?code>/g, '')
+    )
+  )),
+
+  // filtering
+  R.reject(R.propEq('prop', '--*')),
+  R.reject(R.propSatisfies(
+    R.test(/^-(ms|moz|webkit)-/gi),
+    'prop'
+  )),
+  R.reject(R.propSatisfies(
+    R.is(Array),
+    'initial'
+  )),
+
+  R.reduce(
+    (acc, i) => R.merge(
+      acc,
+      R.pipe(...[
+        R.values,
+        R.map(R.of),
+        R.apply(R.zipObj),
+      ])(i)
+    ),
+    {}
+  ),
+]);
+
+const res = fn(mdn);
+
+const result = `module.exports = ${JSON.stringify(res, null, 2)};`;
+
+fs.writeFile(join(__dirname, 'initials.js'), result, 'utf8', (err, res) => {
   if (err) throw err;
   console.log('☯ done');
 })
