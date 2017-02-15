4fb917b6ad56ceffeea9ff383f155ec785fe88de
Vladimir Starkov <iamstarkov@gmail.com>
2017-02-15 17:39:36 +0100

    add reset

---
+++ reset.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'margin': '0',
+  'padding': '0',
+  'border': '0',
+  'font-size': '100%',
+  'font': 'inherit',
+  'vertical-align': 'baseline',
+  'display': 'inline-block',
+  'line-height': '1',
+  'list-style': 'none',
+  'quotes': 'none',
+  'background': 'transparent',
+  'border-collapse': 'collapse',
+  'border-spacing': '0',
+  'text-decoration': 'none',
+  '&:before, &:after': {
+    'content': '',
+    'content': 'none',
+  },
+  ':hover': {
+    'text-decoration': 'none',
+  },
+  '&:focus': {
+    'outline': '0',
+  }
+}
